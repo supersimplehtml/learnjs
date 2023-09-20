d3768b54e66dd840b69b8cb7ca2c6cc69ac3d056
supersimplehtml <128404060+supersimplehtml@users.noreply.github.com>
2023-09-20 17:35:19 +0000

    change

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("Hadi")
+  //************************operations***************************
+  let val= 3;
+  let negval= -val
+  console.log(negval)
+ console.log(3**4)
+  console.log(2*3)
+  console.log(2/5)
+ let str1 = "hello"
+ let str2 = " hadi"
+ let str3 = str1 + str2
+ console.log(str3)
+
